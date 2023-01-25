b6d2fa02e02b3917e93e988de44ea96418c8accf
ArielLopezCuevas <ariellopezcuevas@gmail.com>
2023-01-25 00:55:38 +0000

    Cargamos las bases que vamos a ocupar

--- git_art.R
+++ git_art.R
@@ -8,3 +8,14 @@ library(dplyr)
 library(ggplot2)
 library(tidyverse)
 library(sf)
+
+#Leemos las bases
+
+conteos_semanales_covid_comorbilidades <- read_csv("~/conteos_semanales_covid_comorbilidades.csv")
+
+conteos <- conteos_semanales_covid_comorbilidades %>%  
+  separate(`semana_epi_sintomas\tentidad_res\tmunicipio_res\ttipo_paciente\tdefuncion\tobesidad\tdiabetes\thipertension\tn`, 
+           into = c("semana_epi_sintomas", "entidad_res", "municipio_res", "tipo_paciente", "defuncion", "obesidad", "diabetes", "hipertension", "n"), 
+           sep = "\t")
+
+ensanut <- read_csv("~/R/ensanut_areas_peq.csv")
